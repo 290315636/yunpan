7e99417e4ad92d294911e1db5fa7d6e3ac4eb5d2
zhaochunsheng <290315636@qq.com>
2018-07-12 19:38:40 +0800

    增加自动生成代码

---
+++ src/main/java/com/tikie/file/bean/Test.java
@@ -0,0 +1,58 @@
+package com.tikie.file.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Test implements Serializable {
+    private Long id;
+
+    private String msg;
+
+    private Date ctime;
+
+    private Date utime;
+
+    private String note;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg == null ? null : msg.trim();
+    }
+
+    public Date getCtime() {
+        return ctime;
+    }
+
+    public void setCtime(Date ctime) {
+        this.ctime = ctime;
+    }
+
+    public Date getUtime() {
+        return utime;
+    }
+
+    public void setUtime(Date utime) {
+        this.utime = utime;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note == null ? null : note.trim();
+    }
+}
---
+++ src/main/java/com/tikie/file/mapper/TestMapper.java
@@ -0,0 +1,17 @@
+package com.tikie.file.mapper;
+
+import com.tikie.file.bean.Test;
+
+public interface TestMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    Test selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
+}
