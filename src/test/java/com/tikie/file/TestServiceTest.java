4ac3218aeaa23dd7baece31d9b04048900ce79da
zhaochunsheng <290315636@qq.com>
2018-10-10 14:33:41 +0800

    增加生成主键id的雪花算法

--- src/test/java/com/tikie/file/TestServiceTest.java
+++ src/test/java/com/tikie/file/TestServiceTest.java
@@ -13,6 +13,7 @@ import com.tikie.common.CommonEnums.MQDestination;
 import com.tikie.file.activemq.Producer;
 import com.tikie.file.activemq.Publisher;
 import com.tikie.file.service.TestService;
+import com.tikie.util.IdGen;
 import com.tikie.util.UUIDUtil;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -30,6 +31,13 @@ import java.io.File;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zhaocs
@@ -116,5 +124,34 @@ public class TestServiceTest {
     	for(int i=0;i<10;i++) {
     		publisher.publish("test.topic", "生产消息" + i);
     	}
+    }
+    
+    @Test
+    public void test12() {
+        long avg = 0;
+        for (int k = 0; k < 10; k++) {
+            List<Callable<Long>> partitions = new ArrayList<Callable<Long>>();
+            final IdGen idGen = IdGen.get();
+            for (int i = 0; i < 1400000; i++) {
+                partitions.add(new Callable<Long>() {
+                    @Override
+                    public Long call() throws Exception {
+                        return idGen.nextId();
+                    }
+                });
+            }
+            ExecutorService executorPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            try {
+                long s = System.currentTimeMillis();
+                executorPool.invokeAll(partitions, 10000, TimeUnit.SECONDS);
+                long s_avg = System.currentTimeMillis() - s;
+                avg += s_avg;
+                System.out.println("完成时间需要: " + s_avg / 1.0e3 + "秒");
+                executorPool.shutdown();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("平均完成时间需要: " + avg / 10 / 1.0e3 + "秒");
     }
 }
